8ab1ca16dfa9ed316ecacbf9a166c5d6522f5dd6
abdoulfataoh <abdoulfataoh@gmail.com>
2022-06-26 17:00:51 +0000

    add word, document as granularity options for text extraction

--- utils/pdf.py
+++ utils/pdf.py
@@ -1,10 +1,11 @@
 # coding: utf-8
 
-from dataclasses import dataclass
+
 import logging
+from pathlib import Path
 
 import fitz
-import logging
+from numpy import sort
 
 
 logging.basicConfig(level=logging.INFO)
@@ -16,34 +17,51 @@ class Pdf():
         ...
 
 
-    def extract_pages_text(self, pdf_file_path: str, granularity: str, page_numbers: list = []):
+    def extract_text(self, pdf_file_path: str, granularity: str, page_numbers: list = []):
+        pdf_file_name = Path(pdf_file_path).name
         doc = fitz.open(pdf_file_path)
         data = []
-        if page_numbers == []:
-            for page in doc.pages():
-                data = data + self._extract_text(page, granularity)
+        if granularity == "document":
+            pages_text = ""
+            for page in doc:
+                pages_text = pages_text + "\n\n" + page.get_text()
+            data.append(
+                {
+                    "type": granularity,
+                    "pdf_file_name": pdf_file_name,
+                    "text": pages_text
+
+                }
+            )
             return data
         else:
-            doc_pages = list(doc.pages())
-            for number in page_numbers:
-                number = number - 1 # the first page number is 0
-                page = doc_pages[number]
-                data = data + self._extract_text(page, granularity)
-            return data
+            if page_numbers == []:
+                for page in doc.pages():
+                    data = data + self._extract_text(page, pdf_file_name, granularity)
+                return data
+            else:
+                doc_pages = list(doc.pages())
+                for number in page_numbers:
+                    number = number - 1 # the first page number is 0
+                    page = doc_pages[number]
+                    data = data + self._extract_text(page, pdf_file_name, granularity)
+                return data
 
 
-    def _extract_text(self, page, type: str) -> list:
+    def _extract_text(self, page, pdf_file_name: str, granularity: str) -> list:
         page_number = page.number + 1
-        if type == "page":
+        if granularity == "page":
             page_text = page.get_text()
             return [
                     {
+                        "type": granularity,
+                        "pdf_file_name": pdf_file_name,
                         "page_number": page_number,
-                        "page_text": page_text
+                        "text": page_text
                     }
             ]        
             return page_text
-        elif type == "paragraph":
+        elif granularity == "paragraph":
             paragraphs = []
             blocks = page.get_text("blocks", sort=True)
             for block in blocks:
@@ -52,15 +70,34 @@ class Pdf():
                     paragraphs_number = block[5]
                     paragraphs.append(
                         {
+                            "type": granularity,
+                            "pdf_file_name": pdf_file_name,
                             "page_number": page_number,
                             "paragraphs_number": paragraphs_number,
-                            "paragraphs_text": paragraphs_text
+                            "text": paragraphs_text
                         }
                     )
             return paragraphs
+        elif granularity == "word":
+            words = []
+            word_number = 1
+            for word in page.get_text("words", sort=True):
+                words_text = word[4]
+                word_number = word_number + 1
+                words.append(
+                    {
+                        "type": granularity,
+                        "pdf_file_name": pdf_file_name,
+                        "page_number": page_number,
+                        "word_number": word_number,
+                        "text": words_text
+
+                    }
+                )
+            return words
                        
 
 if __name__ == '__main__':
     pdf = Pdf()
-    r = pdf.extract_pages_text('dataset/pdf/test/test.pdf', "paragraph", [1])
+    r = pdf.extract_text('dataset/pdf/test/test-2.pdf', "document")
     print(r)
