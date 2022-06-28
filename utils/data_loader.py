5610437eea08f3e26fffa3e207286bb6eb7b490c
abdoulfataoh <abdoulfataoh@gmail.com>
2022-06-28 08:57:09 +0000

    add load_clean_dataset methode, fix somes issues

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -4,7 +4,10 @@ from pathlib import Path
 import logging
 import json
 
-from pdf import Pdf
+from utils.pdf import Pdf
+from utils.nlp_toolbox import Tokenizer
+from utils.nlp_toolbox import unidecoder
+from gensim.models.doc2vec import TaggedDocument
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -24,27 +27,49 @@ class DataLoader(object):
         logger.info(f'we found {len(pdf_files_name)} pdf files from <{self.pdf_folder_path}>')
         print(pdf_files_name[0])
         return pdf_files_name
-    
-    
-    def load_data(self, granularity: str) -> list:
+
+
+    def load_cleaned_dataset(
+        self,
+        granularity: str,
+        spacy_lang: str,
+        remove_punctuation: bool,
+        remove_stopword: bool,
+        remove_digit: bool,
+        remove_space: bool,
+        return_tokens: bool
+    ):
+        dataset = self.load_original_data(granularity)
+        return_dataset = []
+        logger.info("load clean dataset")
+        tokenizer = Tokenizer(spacy_lang)
+        for index, item in enumerate(dataset):
+            pdf_file_name = item["pdf_file_name"]
+            remove_items = []
+            logger.info(f"clean {pdf_file_name}")
+            item_text = unidecoder(item["text"])
+            tokens = tokenizer.get_words_tokens(
+                text=item_text,
+                remove_punctuation=remove_punctuation,
+                remove_stopword=remove_stopword,
+                remove_digit=remove_digit,
+                remove_space=remove_space,
+            )
+            
+            if return_tokens:
+                item["text"] = tokens
+            else:
+                item["text"] = " ".join(tokens)
+
+            if type(tokens) is list and tokens != []:
+                return_dataset.append(item)
+
+        return return_dataset
+
+
+    def load_original_data(self, granularity: str) -> list:
         data = []
         for pdf_file_name in self.get_pdf_files_name():
                 logger.info(f'extract {granularity} from {pdf_file_name}')
                 data = data + self.pdf_tool.extract_text(pdf_file_name, granularity)
         return data
-
-
-
-
-if __name__ == "__main__":
-    import json
-
-    data_loader = DataLoader(r'dataset/pdf/test/01')
-    data = data_loader.load_data(granularity='document')
-    print(data)
-    # with open("dataset/cache/dataset-words.json", 'w') as file:
-    #     json.dump(data, file)
-
-
-            
-
