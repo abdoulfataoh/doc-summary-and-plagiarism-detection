13c26e5f42de37df68362e2619fb00978148f36b
abdoulfataoh <abdoulfataoh@gmail.com>
2022-06-29 01:08:21 +0000

    code refactoring

--- statistic.py
+++ statistic.py
@@ -1,26 +1,22 @@
 # coding: utf-8
+# show dataset statistic
 
 import json
 from statistics import mean
 
-def count_words(dataset_cache_file: str):
-    file = open(dataset_cache_file, "r")
-    data = json.load(file)
+from utils.data_loader import DataLoader
+from config import Config
+
+
+def count_words(dataset: str):
     files_name = set()
     count_word_list = []
-    
-    # get all files names
-    for word in data:
+    for word in dataset:  # build  unique file name list
         files_name.add(word["pdf_file_name"])
-
-
-    # convert to list
-    files_name = list(files_name)
-
-    # count words
-    for file_name in files_name:
+    files_name = list(files_name)  # convert files names set to list
+    for file_name in files_name:  # count words
         count_words = 0
-        for word in data:
+        for word in dataset:
             if word["pdf_file_name"] == file_name:
                 count_words = count_words + 1
         count_word_list.append(count_words)
@@ -34,15 +30,14 @@ def count_words(dataset_cache_file: str):
     # printer
     for file_name, count_word in zip(files_name, count_word_list):
         print(f'{count_word} \t :  {file_name}')
-    print("--------------------------------------------------------")
-    print(f"TOTAL : {sum_value}")
-    print(f"MOYENNE : {mean_value}")
-    print(f"MIN : {min_value} ({files_name[count_word_list.index(min_value)]})")
-    print(f"MAX : {max_value} ({files_name[count_word_list.index(max_value)]})")
-    print()
-    
+    print("-----------------------------------------------------------------")
+    print(f"TOTAL: {sum_value}")
+    print(f"MOYENNE: {mean_value}")
+    print(f"MIN: {min_value} ({files_name[count_word_list.index(min_value)]})")
+    print(f"MAX: {max_value} ({files_name[count_word_list.index(max_value)]})")
 
 
 # launch
-count_words("dataset/cache/dataset-words.json")
-
+dataloader = DataLoader(Config.DATASET_PATH)
+dataset = dataloader.load_original_data("word")
+count_words(dataset)
