26c48ec597bae0f9c65731f7da51219bdf445746
abdoulfataoh <abdoulfataoh@gmail.com>
2022-07-06 11:49:47 +0000

    add others folder path

--- config.py
+++ config.py
@@ -7,5 +7,6 @@ class Config():
     DATASET_PATH = Path(r"dataset/pdf/")
     TRAIN_DATASET_PATH = Path(r"assets/datasets")
     TRAIN_MODELS_PATH = Path(r"assets/models")
+    OTHERS_DATA_PATH = Path(r"assets/others")
     SPACY_MODEL = "fr_core_news_sm"
     WORKERS = 2
