66ca30170729b41b7905694f5fe3c9957af7831b
abdoulfataoh <abdoulfataoh@gmail.com>
2022-07-06 22:01:23 +0000

    add embeddings path

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Config():
     DATASET_PATH = Path(r"dataset/pdf/")
     TRAIN_DATASET_PATH = Path(r"assets/datasets")
     TRAIN_MODELS_PATH = Path(r"assets/models")
+    EMBEDDINGS_PATH = Path(r"assets/embeddings")
     OTHERS_DATA_PATH = Path(r"assets/others")
     SPACY_MODEL = "fr_core_news_sm"
     WORKERS = 2
