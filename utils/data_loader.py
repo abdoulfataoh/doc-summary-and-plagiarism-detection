be6ba2d12686bd38fe39074df31547acb9971025
abdoulfataoh <abdoulfataoh@gmail.com>
2022-08-15 10:19:57 +0000

    refactoring data_loader

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,126 +1,109 @@
 # coding: utf-8
 
-from pathlib import Path
 import logging
+from pathlib import Path
+from typing import List, Union, Dict
 
 from utils.pdf import Pdf
-from utils.nlp_toolbox import Tokenizer
-from utils.nlp_toolbox import unidecoder
-from gensim.models.doc2vec import TaggedDocument
+from utils.nlp import TextCleaner
+from utils.enumerations import Granularity
+from utils.entities import Document, Page, Paragraph
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-class DataLoader(object):
+class DataLoader:
 
-    def __init__(self, pdfs_path: str) -> None:
-        self.pdfs_path = pdfs_path
-        self.pdf_tool = Pdf()
+    _files_path: str
+    _pdf: Pdf
+    _text_cleaner: TextCleaner
+    _listed_files: List[str]
 
-    def get_pdf_files_name(self) -> list:
-        pdf_files_name = []
-        if Path(self.pdfs_path).is_file():
-            logger.info(
-                f'we found one (01) pdf file from <{self.pdfs_path}>'
-            )
-            pdf_files_name.append(self.pdfs_path)
-        elif Path(self.pdfs_path).is_dir():
-            for pdf_file_name in Path(self.pdfs_path).glob("*.pdf"):
-                pdf_files_name.append(pdf_file_name)
-            logger.info(
-                f'we found {len(pdf_files_name)} pdf files from <{self.pdfs_path}>'
-            )
-            print(pdf_files_name[0])
-        return pdf_files_name
-
-    def load_cleaned_dataset(
+    def __init__(
         self,
-        dataset: list,
-        spacy_lang: str,
+        files_path: str,
+        pdf: Pdf,
+        text_cleaner: TextCleaner
+    ) -> None:
+
+        self._files_path = files_path
+        self._pdf = pdf
+        self._text_cleaner = text_cleaner
+        self._listed_files = self._listing_pdf_files(files_path)
+
+        if not self._listed_files:
+            raise Exception("No pdf file detected")
+            logger.warning(f"Unable de find pdf file from {files_path}")
+
+    def load_data(
+        self,
+        granularity: Granularity
+    ) -> List[Dict[str, Union[Document, Page, Paragraph]]]:
+
+        dataset = []
+        for pdf_path in self._listed_files:
+            log_msg = f"load {pdf_path} data ..."
+            data = {pdf_path: self._pdf.extract_text(pdf_path, granularity)}
+            dataset.append(data)
+            logger.info(log_msg)
+
+        return dataset
+
+    def clean_data(
+        self,
+        dataset: List[Union[Document, Page, Paragraph]],
+        spacy_lang_model: str,
         remove_punctuation: bool,
         remove_stopword: bool,
         remove_digit: bool,
         remove_space: bool,
-        return_tokens: bool
-    ):
-        return_dataset = []
-        logger.info("load clean dataset")
-        tokenizer = Tokenizer(spacy_lang)
-        for index, item in enumerate(dataset):
-            pdf_file_name = item["pdf_file_name"]
-            item_text = unidecoder(item["text"])
-            tokens = tokenizer.get_words_tokens(
-                text=item_text,
-                remove_punctuation=remove_punctuation,
-                remove_stopword=remove_stopword,
-                remove_digit=remove_digit,
-                remove_space=remove_space,
-            )
-            if return_tokens:
-                item["text"] = tokens
-            else:
-                item["text"] = " ".join(tokens)
+        return_word_list: bool
+    ) -> List[Union[Document, Page, Paragraph]]:
 
-            if type(tokens) is list and tokens != []:
-                return_dataset.append(item)
-
-        return return_dataset
-
-    def load_original_data(self, granularity: str) -> list:
-        data = []
-        for pdf_file_name in self.get_pdf_files_name():
-            logger.info(f'extract {granularity} from {pdf_file_name}')
-            data = data + self.pdf_tool.extract_text(pdf_file_name, granularity)
-        return data
-
-    @staticmethod
-    def word2vec_tag_doc(dataset: list) -> list:
-        granularity = dataset[0]["type"]
-        tagged_doc = []
-
-        logger.info(f"we will tag {granularity} by {granularity}")
-
-        if granularity == "paragraph":
-            for paragraph in dataset:
-                text = paragraph["text"]
-                tagged_doc.append(
-                    TaggedDocument(
-                        words=text,
-                        tags=[
-                                (
-                                    paragraph["pdf_file_name"],
-                                    paragraph["page_number"],
-                                    paragraph["paragraphs_number"]
-                                )
-                        ]
-                    )
+        for data in dataset:
+            data_name = [key for key in data.keys()].pop()
+            data_value = [value for value in data.values()].pop()
+            log_msg = f"clean {data_name} ..."
+            logger.info(log_msg)
+            for entity in data_value:
+                entity_text = entity.text
+                clean_entity_text = self._text_cleaner.clean(
+                    text=entity_text,
+                    remove_punctuation=remove_punctuation,
+                    remove_stopword=remove_stopword,
+                    remove_digit=remove_digit,
+                    remove_space=remove_space,
+                    return_word_list=return_word_list
                 )
-        elif granularity == "page":
-            for page in dataset:
-                text = page["text"]
-                tagged_doc.append(
-                    TaggedDocument(
-                        words=text,
-                        tags=[
-                                (
-                                    page["pdf_file_name"],
-                                    page["page_number"],
-                                )
-                        ]
-                    )
-                )
-        elif granularity == "document":
-            for doc in dataset:
-                text = doc["text"]
-                tagged_doc.append(
-                    TaggedDocument(
-                        words=text,
-                        tags=[
-                                (
-                                    doc["pdf_file_name"]
-                                )
-                        ]
-                    )
-                )
-        return tagged_doc
+                entity.cleaned_text = clean_entity_text
+
+    def _listing_pdf_files(self, files_path: Union[str, Path]) -> List[str]:
+        files_path = Path(files_path)
+        logger.info(f"Listing pdf files from {files_path} ...")
+        if files_path.is_file():
+            listed_files = [str(files_path)]
+            log_msg = "We found 1 pdf file"
+
+        elif files_path.is_dir():
+            listed_files = [str(path) for path in files_path.glob("*.pdf")]
+            log_msg = f"We found {len(listed_files)} pdf files"
+
+        logger.info(log_msg)
+
+        return listed_files
+
+    @property
+    def text_cleaner(self) -> TextCleaner:
+        return self._text_cleaner
+
+    @text_cleaner.setter
+    def text_cleaner(self, text_cleaner) -> bool:
+        logger.info("set new text_cleaner object")
+        self._text_cleaner = text_cleaner
+        return True
+
+    @text_cleaner.deleter
+    def text_cleaner(self) -> bool:
+        logger.warning("deleting text cleaner is not allowed")
+        return False
