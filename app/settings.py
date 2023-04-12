ae32f97ec31adf75d11512bcc256c82a4d62946b
abdoulfataoh <abdoulfataoh@gmail.com>
2023-04-12 14:42:24 +0000

    use Patch object insteadof str for path

--- app/settings.py
+++ app/settings.py
@@ -14,14 +14,14 @@ env.read_env()
 TEST = env.bool('TEST', False)
 
 # [ Path settings ]
-ASSETS_FOLDER = env('ASSETS_FOLDER', r'assets')
+ASSETS_FOLDER = env('ASSETS_FOLDER', Path('assets'))
 CACHE_FOLDER = env('CACHE_FOLDER', Path(ASSETS_FOLDER, 'cache'))
 DATASET_FOLDER = env('DATASET_FOLDER', Path(ASSETS_FOLDER, 'dataset'))
 TRAIN_DATASET_FOLDER = env('TRAIN_DATASET_FOLDER', Path(DATASET_FOLDER, 'train'))  # noqa: E501
 TEST_DATASET_FOLDER = env('TEST_DATASET_FOLDER', Path(DATASET_FOLDER, 'test'))
 MODELS_FOLDER = env('MODELS_FOLDER', Path(ASSETS_FOLDER, 'models'))
 EMBEDDINGS_FOLDER = env('EMBEDDINGS_FOLDER', Path(ASSETS_FOLDER, 'embeddings'))
-METRICS_FOLDER = env('METRICS_FOLDER', 'metrics')
+METRICS_FOLDER = env('METRICS_FOLDER', Path('metrics'))
 
 # [ Spacy settings ]
 SPACY_MODEL_NAME = env('SPACY_MODEL', 'fr_core_news_sm')
